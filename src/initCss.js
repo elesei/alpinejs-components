0bf46cba6e5100b8a94ee9af69486610d1b49514
Elesei <elesei@elesei.com>
2024-07-28 15:22:30 +0100

    fix error CSS_PUBLIC_PATH

--- src/initCss.js
+++ src/initCss.js
@@ -1,8 +1,9 @@
 export function initCss(shadowDom) {
-    if (CSS_PUBLIC_PATH !== undefined) {
+    try {
         const link = document.createElement('link');
         link.rel = 'stylesheet';
         link.href = CSS_PUBLIC_PATH;
         shadowDom.appendChild(link);
+    } catch (e) {
     }
 }
